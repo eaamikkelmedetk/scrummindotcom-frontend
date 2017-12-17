c062864d4e088d8de2fdd0279145099a800c30c4
DESKTOP-JH6TVNS\Mikkel <mikkelkaasandersen@gmail.com>
2017-12-17 20:13:09 +0100

    v0.1.10 Remove Service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Root from "./root";
 import { store } from "./Store/store";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
-registerServiceWorker();
