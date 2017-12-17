c7544ae6e9adc456b171fc90140797e4130fd900
DESKTOP-JH6TVNS\Mikkel <mikkelkaasandersen@gmail.com>
2017-12-17 20:22:12 +0100

    v.0.1.11 Unregister serviceworker

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Root from "./root";
 import { store } from "./Store/store";
+import { unregister } from "./registerServiceWorker";
 
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
+unregister();
